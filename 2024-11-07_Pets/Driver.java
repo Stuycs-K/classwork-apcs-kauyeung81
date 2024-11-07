027bbbd08d0b098cf8adbe97ca9fa3196adbe673
Karen Au-Yeung <kau-yeung60@stuy.edu>
2024-11-07 11:42:38 -0500

    overwrote getName() and added comment

--- 2024-11-07_Pets/Driver.java
+++ 2024-11-07_Pets/Driver.java
@@ -1,3 +1,6 @@
+// When getName(); was overwritten, the speak method in the Bird class now calls the new getName();
+// Instead of returning NAME, it returns The Mighty NAME
+
 public class Driver{
 
   public static void main(String[] args){

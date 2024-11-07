7de915c636f4eee830f6fa13955cdeac854fb864
Karen Au-Yeung <kau-yeung60@stuy.edu>
2024-11-07 11:33:01 -0500

    Overwrote speak() in Bird.java

--- 2024-11-07_Pets/Bird.java
+++ 2024-11-07_Pets/Bird.java
@@ -25,8 +25,19 @@ public class Bird extends Animal {
     I am ? inches tall
   */
   //You can call the old method using super.speak();
-  //public void speak(){}
+  public void speak(){
+    super.speak();
+    System.out.println("My feathers are colored " + getColor());
+    System.out.println("I am " + getHeight() + " inches tall");
+  }
 
+  public String getColor(){
+    return color;
+  }
+
+  public double getHeight(){
+    return height;
+  }
 
 
   //7:Override the getName() method so that a bid's name is "The Mighty NAME" instead of "NAME"

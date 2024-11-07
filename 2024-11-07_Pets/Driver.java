1b09c65049b17bec712287aaf447e8dae26e4ba5
Karen Au-Yeung <kau-yeung60@stuy.edu>
2024-11-07 11:26:43 -0500

    tested Animal constructor and speak()

--- 2024-11-07_Pets/Driver.java
+++ 2024-11-07_Pets/Driver.java
@@ -1,6 +1,8 @@
 public class Driver{
-  public static void main(String[] args){
 
+  public static void main(String[] args){
+    Animal cat = new Animal("meow", 3, "Kitty");
+    cat.speak();
   }
 
 }

9fa21b72ed39feee66a6e78df0bbc64f24b18fdf
Karen Au-Yeung <kau-yeung60@stuy.edu>
2024-11-07 11:30:17 -0500

    Added bird constructor and tested

--- 2024-11-07_Pets/Driver.java
+++ 2024-11-07_Pets/Driver.java
@@ -3,6 +3,9 @@ public class Driver{
   public static void main(String[] args){
     Animal cat = new Animal("meow", 3, "Kitty");
     cat.speak();
+
+    Animal bird = new Bird("squawk", 5, "Bird", 3.0, "blue");
+    bird.speak();
   }
 
 }

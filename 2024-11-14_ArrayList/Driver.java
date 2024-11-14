91e03054fd85cb42d86779b0f43f8d954c6c5589
Karen Au-Yeung <Karen@Leos-MBP.lan>
2024-11-14 17:12:06 -0500

    Finished mixLists method, added test cases

---
+++ 2024-11-14_ArrayList/Driver.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class Driver{
+  public static void main(String[] args){
+		
+		System.out.println("Testing createRandomArray");
+    System.out.println(ArrayListPractice.createRandomArray(10).toString());
+		
+    ArrayList<String> temp1 = ArrayListPractice.createRandomArray(10);
+    ArrayListPractice.replaceEmpty(temp1);
+    System.out.println("\ntemp1 after replaceEmpty: " + temp1.toString());
+    System.out.println("temp3 makeReversedList: " + ArrayListPractice.makeReversedList(temp1).toString());
+
+    ArrayList<String> temp2 = ArrayListPractice.createRandomArray(14);
+    ArrayListPractice.replaceEmpty(temp2);
+    System.out.println("\ntemp2 after replaceEmpty: " + temp2.toString());
+    System.out.println("temp3 makeReversedList: " + ArrayListPractice.makeReversedList(temp2).toString());
+
+    ArrayList<String> temp3 = ArrayListPractice.createRandomArray(7);
+    ArrayListPractice.replaceEmpty(temp3);
+    System.out.println("\ntemp3 after replaceEmpty: " + temp3.toString());
+    System.out.println("temp3 makeReversedList: " + ArrayListPractice.makeReversedList(temp3).toString());
+
+		System.out.println("\nTesting mixLists");
+		System.out.println("mixLists(temp1, temp2): " + ArrayListPractice.mixLists(temp1, temp2));
+		System.out.println("mixLists(temp2, temp3): " + ArrayListPractice.mixLists(temp2, temp3));
+		System.out.println("mixLists(temp1, temp3): " + ArrayListPractice.mixLists(temp1, temp3));
+
+  }
+}

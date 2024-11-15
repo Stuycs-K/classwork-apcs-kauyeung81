a0703ca9bb6e9f43be1c46cb355c5c27aa5000a0
Karen Au-Yeung <Karen@Leos-MBP.lan>
2024-11-14 23:07:52 -0500

    Added more test cases

--- 2024-11-14_ArrayList/Driver.java
+++ 2024-11-14_ArrayList/Driver.java
@@ -20,11 +20,19 @@ public class Driver{
     ArrayListPractice.replaceEmpty(temp3);
     System.out.println("\ntemp3 after replaceEmpty: " + temp3.toString());
     System.out.println("temp3 makeReversedList: " + ArrayListPractice.makeReversedList(temp3).toString());
+		
+		ArrayList<String> temp4 = ArrayListPractice.createRandomArray(200000);
+		ArrayListPractice.replaceEmpty(temp4);
+    System.out.println("\ntemp4 after replaceEmpty: " + temp4.toString());
+    System.out.println("temp4 makeReversedList: " + ArrayListPractice.makeReversedList(temp4).toString());
 
 		System.out.println("\nTesting mixLists");
 		System.out.println("mixLists(temp1, temp2): " + ArrayListPractice.mixLists(temp1, temp2));
-		System.out.println("mixLists(temp2, temp3): " + ArrayListPractice.mixLists(temp2, temp3));
 		System.out.println("mixLists(temp1, temp3): " + ArrayListPractice.mixLists(temp1, temp3));
+		System.out.println("mixLists(temp1, temp4): " + ArrayListPractice.mixLists(temp1, temp4));
+		System.out.println("mixLists(temp2, temp3): " + ArrayListPractice.mixLists(temp2, temp3));
+		System.out.println("mixLists(temp2, temp4): " + ArrayListPractice.mixLists(temp2, temp4));
+		System.out.println("mixLists(temp3, temp4): " + ArrayListPractice.mixLists(temp3, temp4));
 
   }
 }

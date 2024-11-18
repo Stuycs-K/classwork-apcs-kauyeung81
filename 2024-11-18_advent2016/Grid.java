6c295b939b56b9846ff38051f833aea979a3b99f
Karen Au-Yeung <kau-yeung60@stuy.edu>
2024-11-18 10:44:30 -0600

    Starting day 1

--- 2024-11-18_advent2016/Grid.java
+++ 2024-11-18_advent2016/Grid.java
@@ -4,7 +4,27 @@ import java.util.Scanner;
 
 public class Grid{
 
+  public static int move(String move, int direction){
+    
+  }
 
+  public static int distance(String filename) {
+  		try {
+  			File file = new File(filename);
+  			Scanner input = new Scanner(file);
+  			int xCord;
+        int yCord;
+        int direction = 1;
+
+
+
+
+  			input.close();
+  			return count;
+  		} catch (FileNotFoundException e) {
+  			return -1;
+  		}
+  	}
 
 
 }

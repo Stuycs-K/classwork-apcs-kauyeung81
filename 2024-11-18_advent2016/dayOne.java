5964bf776db6b6ebfb048aa3b31993c124714fb5
Karen Au-Yeung <Karen@Leos-MBP.lan>
2024-11-20 01:08:42 -0500

    Working on dayOne

--- 2024-11-18_advent2016/dayOne.java
+++ 2024-11-18_advent2016/dayOne.java
@@ -1,31 +1,72 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.io.*; 
+import java.util.*; 
 
 public class dayOne{
 
-  public static int solve(String movement, int d){
-    int direction = d;
-    if (movement.charAt(0) == 'L'){
-      direction--;
+  public static int solve(String[] movement){
+    int direction = 0;
+		int xCord = 0;
+		int yCord = 0;
+		int[] position = new int[]{xCord, yCord};
+		int[] dir = new int[]{1,0};
+		int moveset = 0;
+		
+    for (int i = 0; i < movement.length; i++){
+			int move = Integer.parseInt(String.valueOf(movement[i].charAt(1)));
+    	if (movement[i].charAt(0) == 'L'){
+    		direction--;
+    	}
+    	if (movement[i].charAt(0) == 'R'){
+    		direction++;
+    	}
+			
+			direction = Math.abs(direction) % 3;
+			if (direction == 0){
+				move = move * 1;
+				yCord += move;
+			}
+			if (direction == 1){
+				move = move * 1;
+				xCord += move;
+			}
+			if (direction == 2){
+				move = move * -1;
+				yCord += move;
+			}
+			if (direction == 3){
+				move = move * -1;
+				xCord += move;
+			}
+			position = new int[]{xCord, yCord};
+			System.out.println(Arrays.toString(position));
     }
-    if (movement.charAt(0) == 'R'){
-      direction++;
-    }
-		return direction;
+		
+		moveset += position[0];
+		moveset += position[1];
+		return moveset;
   }
 
 	public static int distance(String filename) {
   		try {
         File file = new File(filename);
   			Scanner input = new Scanner(file);
-        int x = 0; y = 0; direction = 0;
-
-        while (input.hasNext()){
-          String movement = input.next();
-          direction = solve(movement, direction);
-
-        }
+				String in = input.nextLine();
+				String[] instructions = in.split(", ");
+				input.close();
+				
+				int[] position = new int[]{0, 0};
+				int[] direction = new int[]{1,0};
+				
+        int x = 0;
+				int y = 0;
+				int output = 0;
+				
+				output = solve(instructions);
+				
+				return output;
 
   		} catch (FileNotFoundException e) {
   			return -1;
@@ -33,7 +74,7 @@ public class dayOne{
 		}
 
 	public static void main(String[] args){
-    System.out.println()
+    System.out.println(distance("adventDay1.txt"));
 	}
 
 

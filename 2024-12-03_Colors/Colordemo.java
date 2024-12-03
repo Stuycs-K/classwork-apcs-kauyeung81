acbbac3de20e891b3b5e49f57424792d8f6f1ee1
Karen Au-Yeung <kau-yeung60@stuy.edu>
2024-12-03 10:55:26 -0600

    Colors classwork + homework

---
+++ 2024-12-03_Colors/Colordemo.java
@@ -0,0 +1,32 @@
+public class Colordemo{
+
+  public static final String CLEAR_SCREEN =  "\u001b[2J";
+  public static final String HIDE_CURSOR =  "\u001b[?25l";
+  public static final String SHOW_CURSOR =  "\u001b[?25h";
+
+  public static int BLACK = 30;
+  public static int RED = 31;
+
+  public static void color(int foreground,int background){
+    System.out.print( "\u001b[" + foreground + ";" + (10+background) + "m");
+  }
+
+  public static void color(int foreground, int background, int modifier){
+    System.out.print("\u001b[" + foreground + ";" + (10+background) + ";" + modifier + "m");
+  }
+
+  public static void main(String[] args){
+    for(int r = 0; r < 256; r+=32){
+      for(int g = 0; g <= 256; g+=32){
+        for(int b = 0; b <= 256; b+=32){
+          System.out.print("\u001b[38;2;"+r+";"+g+";"+b+";7m.");
+        }
+      }
+    System.out.println();
+    }
+
+  }
+
+
+
+}

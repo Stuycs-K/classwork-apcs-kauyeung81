e2e9c11f3c9b44fdc24d14140ff7a2fb71fd781f
Karen Au-Yeung <kau-yeung60@stuy.edu>
2025-02-04 10:15:49 -0500

    Stuff

---
+++ 2025-02-09-recursion/Backtracking.java
@@ -0,0 +1,22 @@
+public class Backtracking{
+
+  /*
+  *@param length how long the words must be
+  *param word the variable to store the partial solution (Initial call should start at "")
+  *param letters will contain the unique* letters allowed in the string.
+  *      (*unique: You can assume there are no duplicates)
+  *@return the number of words that have no adjacent matching letters
+  *        using the letters provided. This is possibly zero if there
+  *        is only one unique letter provided, but that is an edge case.
+  *Repetition allowed except when letters are adjacent e.g. "abababa" is allowed.
+  */
+  public static long countNoDoubleLetterWords(int length,String word, String letters){
+  //Hint: not a wrapper method, but you must call it starting with "" as your word
+  // e.g. countNoDoubleLetterWords(5,"","abc")
+  }
+
+  public static boolean groupSum(int start, int[] nums, int target) {
+    return true;
+  }
+
+}

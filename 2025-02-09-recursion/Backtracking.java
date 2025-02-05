edfb38606874a0529d5148fb60d9a4db08324273
Karen Au-Yeung <Karen@Leos-MBP.lan>
2025-02-05 03:44:50 -0500

    Finished countNoDoubleLetterWords

--- 2025-02-09-recursion/Backtracking.java
+++ 2025-02-09-recursion/Backtracking.java
@@ -13,18 +13,35 @@ public class Backtracking{
   public static long countNoDoubleLetterWords(int length,String word, String letters){
   //Hint: not a wrapper method, but you must call it starting with "" as your word
   // e.g. countNoDoubleLetterWords(5,"","abc")
+	if (length == 0){
+		return 1;
   }
+	int count = 0;
+	for (int i = 0; i < letters.length(); i++){
+		char added = letters.charAt(i);
+		if (word.equals("") || word.charAt(word.length() -1) != added){
+			count += countNoDoubleLetterWords(length-1, word + added, letters);
+		}
+	}
+	return count;
+}
 
   public static boolean groupSum(int start, int[] nums, int target) {
-    for (int i = 0; i < nums.length; i++){
-      if (start + nums[i] == target){
-        return true;
-      }
-      else{
-        return groupSum(start+nums[i], nums)
-      }
-    }
-    return false;
+		if (start >= nums.length){
+			return target == 0;
+		}
+    return groupSum(start+1, nums, target - nums[start]) || groupSum(start+1, nums, target);
   }
+	
+	public static void main(String args[]){
+		System.out.println(groupSum(0, new int[]{2, 4, 8}, 10));
+		System.out.println(groupSum(0, new int[]{2, 4, 8}, 14));
+		System.out.println(groupSum(0, new int[]{2, 4, 8}, 9));
+		
+		System.out.println(countNoDoubleLetterWords(5,"","abc"));
+		System.out.println(countNoDoubleLetterWords(2,"","a"));
+		System.out.println(countNoDoubleLetterWords(2,"","ab"));
+		System.out.println(countNoDoubleLetterWords(3,"","ab"));
+	}
 
 }
